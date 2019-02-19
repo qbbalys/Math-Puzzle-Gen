9329b55538dbdad05c1b5eb7a54272b44600d513
qbbalys <qbba.lys@gmail.com>
2019-02-19 22:55:22 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,83 @@
+var puzzles = [];
+
+function randomInt(min, max) { // [min, max)
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generatePuzzles() {
+    puzzles = [];
+
+    for (var i = 0; i < 9; ++i) {
+        puzzles.push(new Puzzle(15));
+    }
+
+    displayPuzzles();
+}
+
+function displayPuzzles() {
+    var solved = document.getElementById("solvedCheckbox").checked;
+    var html = "";
+    
+    for (var i = 0; i < puzzles.length; ++i) {
+        html += puzzles[i].render(solved);
+    }
+
+    document.getElementById("puzzles").innerHTML = html;
+}
+
+class Puzzle {
+
+    constructor(maxValue) {
+        this.board = initialBoard;
+        this.setupValues(maxValue);
+        this.mask = masks[randomInt(0, masks.length)];
+    }
+
+    setupValues(maxValue) {
+        this.board[0][0] = randomInt(0, maxValue);
+        this.board[0][2] = randomInt(0, this.board[0][0]);
+        this.board[0][4] = this.board[0][0] - this.board[0][2];
+        
+        this.board[4][4] = randomInt(this.board[0][4], maxValue);
+        this.board[2][4] = this.board[4][4] - this.board[0][4];
+
+        this.board[2][2] = randomInt(0, maxValue);
+        this.board[2][0] = this.board[2][4] + this.board[2][2];
+        this.board[4][0] = this.board[0][0] + this.board[2][0];
+        this.board[4][2] = this.board[0][2] + this.board[2][2];
+    }
+
+    render(solved) {
+        var html = '<table class="puzzle">';
+
+        for (var row = 0; row < this.board.length; row++) {
+            html += '<tr>';
+
+            for (var col = 0; col < this.board[row].length; col++) {
+                var data = this.board[row][col];
+                var visible = this.mask[row][col];
+
+                if (data == null) {
+                    html += '<td class="filled" />';
+                } else {
+                    if (solved) {
+                        html += visible ? '<td>' : '<td class="solved">';
+                        html += data;
+                        html += '</td>';
+                    } else {
+                        html += '<td>';
+                        html += visible ? data : "";
+                        html += '</td>';
+                    }
+                }
+            }
+
+            html += '</tr>';
+        }
+
+        html += '</table>';
+        return html;
+    }
+}
